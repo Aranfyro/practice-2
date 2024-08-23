1bf9dd70dbb871b71650d0f441d3043c24afb0ec
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-08-23 13:01:39 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# Практическое задание по уроку "Строки и индексация строк":
-example = 'Переменная'
-print(example[0])
-print(example[-1])
-print(example[5:])
-print(example[::-1])
-print(example[::2])
