10a4d99854584d20124fc420e707d6712ae5d7b8
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-08-23 12:32:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Практическое задание по уроку "Строки и индексация строк":
+example = 'Переменная'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[::2])
